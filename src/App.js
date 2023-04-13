8058d2a46c105788110c29a3ee732611835ef8f0
Stefan Zerox <stefanveljkovic@yahoo.com>
2023-04-13 16:34:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
+
+const App = () => {
+  const queryClient = useQueryClient();
+  console.log(POSTS);
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+
+  return (
+    <div>
+      {postsQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+};
+
+const wait = (duration) => {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+};
+
+export default App;
